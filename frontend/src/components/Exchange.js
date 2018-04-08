b0c222bdffce77fb2f543939c5098d81e4a488ee
Jules Choquart <julochoq@gmail.com>
2018-04-08 16:52:19 -0700

    Search component (#84)
    * node-isbn implemented
    
    implemented node-isbn. user inputs isbn, page pulls author, title, publisher, published date, as well as a thumbnail for the book, all from the google-books-api
    
    * added price and condition inputs
    
    added price text input and condition dropdown menu.
    still need to add validation for price input and do overall formatting for the page
    
    * slight formatting tweaks
    
    moved data to card component from material UI. still need to debug drop-down menu and add validation
    
    * added quick fix
    
    fixed the situation where node-isbn does not provide a thumbnail
    
    * Fixed code formatting and merge conflicts
    
    * packaga.json fix
    
    * final touches
    
    * Fixd style errors
    
    * changed dollar sign position
    
    * deleted duplicate file
    
    * Update Sell.js
    
    * Created search bar component that passes state up to exchange.

--- frontend/src/components/Exchange.js
+++ frontend/src/components/Exchange.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
 import Page from './Page';
 import BookFeed from './BookFeed';
+import SearchBar from 'material-ui-search-bar';
 
 class Exchange extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchValue: '',
+    };
+  }
+
   render() {
     return (
       <Page>
         <h1>Exchange</h1>
+
+        <SearchBar
+          value={this.state.value}
+          onChange={(newValue) => this.setState({ searchValue: newValue })}
+          onRequestSearch={() => console.log(this.state.searchValue)}
+          style={{
+            margin: '0 auto',
+            maxWidth: 800
+          }}
+        />
+
         <BookFeed query={{}} />
       </Page>
-
     );
   }
 }
