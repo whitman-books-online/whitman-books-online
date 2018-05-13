5a0158dd0bd05af1352a79a776b2cee23f5ffc5e
Isaiah Banta <bantaib@whitman.edu>
2018-05-12 22:26:11 -0700

    Update App.js

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -21,7 +21,7 @@ import {cyan500} from 'material-ui/styles/colors';
 
 import DocumentTitle from 'react-document-title'
 import Favicon from 'react-favicon';
-import {logo} from './logo-small.png';
+import {logo} from './logo.png';
 import Helmet from 'react-helmet';
 
 // Create a history of your choosing
