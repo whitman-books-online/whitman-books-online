9f7053c0811ce3ce9484a0a01599b6ec4dc0df1a
Richard Farman <farmanrl@whitman.edu>
2018-05-14 01:34:08 -0700

    Farmanrl fix (#186)
    * Update selectors.js
    
    * Update selectors.js

--- frontend/src/redux/users/selectors.js
+++ frontend/src/redux/users/selectors.js
@@ -2,12 +2,12 @@ import { createSelector } from 'reselect';
 
 const getUserList = state => state.usersReducer.userList;
 
-const getUserId = (state, props) => props.userId;
+const getUserId = (state, props) => props.google_tok;
 
 export const getUserById = createSelector(
   [getUserList, getUserId],
   (userList, userId) => {
-    const user = userList.find(item => item.googleId === userId);
+    const user = userList.find(item => item.google_tok === userId);
     return user;
   },
 );
